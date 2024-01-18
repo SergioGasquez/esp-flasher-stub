d50c836e423366cea66addad5463ebe2d1acda95
Sergio Gasquez <sergio.gasquez@gmail.com>
2024-01-18 14:49:50 +0100

    docs: Add docstrings

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,9 @@
 // Size of sesponse without data reference
 pub const RESPONSE_SIZE: usize = 10;
 
+/// Error Codes
+///
+/// See https://github.com/espressif/esptool/blob/67a91cbfef54f281212951b8226583ba3c1d0a85/flasher_stub/include/stub_flasher.h#L95
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Error {
@@ -25,6 +28,9 @@ pub enum Error {
     EraseErr = 0x36, // TODO: Is it OK to add custom Error?
 }
 
+/// Command identifier
+///
+/// https://docs.espressif.com/projects/esptool/en/latest/esp32c3/advanced-topics/serial-protocol.html#supported-by-stub-loader-and-rom-loader
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum CommandCode {
@@ -52,45 +58,66 @@ pub enum CommandCode {
     FlashEncryptedData = 0xD4,
 }
 
+/// Command packet header
+///
+/// See https://docs.espressif.com/projects/esptool/en/latest/esp32c2/advanced-topics/serial-protocol.html#id2
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct CommandBase {
+    /// Direction of the packet. Always 0x00 for commands.
     pub direction: Direction,
+    /// Command identifier
     pub code: CommandCode,
+    /// Length of Data field, in bytes.
     pub size: u16,
+    /// Simple checksum of part of the data field (only used for _DATA commands)
     pub checksum: u32,
 }
 
+/// SYNC command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct SyncCommand {
     pub base: CommandBase,
+    /// 36 bytes: 0x07 0x07 0x12 0x20, followed by 32 x 0x55
     pub payload: [u8; 36],
 }
 
+/// _BEGIN commands input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct BeginCommand {
     pub base: CommandBase,
+    /// Size to erase
     pub total_size: u32,
+    /// Number of data packets
     pub packet_count: u32,
+    /// Data size in one packet
     pub packet_size: u32,
+    /// Flash offset
     pub offset: u32,
+    // In ROM, there also a field to begin encrypted flash.
 }
 
+/// _DATA commands input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct DataCommand {
     pub base: CommandBase,
+    /// Data size
     pub size: u32,
+    /// Sequence number
     pub sequence_num: u32,
+    /// 0, 0
     pub reserved: [u32; 2],
 }
 
+/// _END commands input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct EndFlashCommand {
     pub base: CommandBase,
+    /// 0 means reboot, 1 to run user code.
     reboot: bool, // private to ensure should_reboot is used.
 }
 
@@ -102,109 +129,166 @@ impl EndFlashCommand {
     }
 }
 
+/// MEM_END command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct MemEndCommand {
     pub base: CommandBase,
+    /// Execute flag
     pub stay_in_stub: u32,
+    /// Entry point address
     pub entrypoint: fn(),
 }
 
+/// WRITE_REG command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct WriteRegCommand {
     pub base: CommandBase,
+    /// Address
     pub address: u32,
+    /// Value
     pub value: u32,
+    /// Mask
     pub mask: u32,
+    /// Delay in microseconds
     pub delay_us: u32,
 }
 
+/// READ_REG command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct ReadRegCommand {
     pub base: CommandBase,
+    /// Address
     pub address: u32,
 }
 
-// Possibly move to other module
+/// Parameters of the attached SPI flash chip (sizes, etc).
+///
+/// See https://github.com/espressif/esptool/blob/16e4faeeaa3f95c6b24dfdcc498ffc33924d5f5f/esptool/loader.py#L1214
+// TODO: Possibly move to other module
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct SpiParams {
+    /// Flash chip ID
     pub id: u32,
+    /// Total size in bytes
     pub total_size: u32,
+    /// Block size
     pub block_size: u32,
+    /// Sector size
     pub sector_size: u32,
+    /// Page size
     pub page_size: u32,
+    /// Status mask
     pub status_mask: u32,
 }
 
+/// SPI_SET_PARAMS command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct SpiSetParamsCommand {
     pub base: CommandBase,
+    /// SPI parameters
     pub params: SpiParams,
 }
 
+// TODO: SpiAttachCommand  (ROM)
+// TODO: SpiAttachStubCommand ? (STUB)
+
+/// CHANGE_BAUDRATE command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct ChangeBaudrateCommand {
     pub base: CommandBase,
+    /// New baud rate
     pub new: u32,
+    /// 0 if we are talking to the ROM loader or the current/old baud rate if we
+    /// are talking to the stub loader.
     pub old: u32,
 }
 
+/// SPI_FLASH_MD5 command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct SpiFlashMd5Command {
     pub base: CommandBase,
+    // Address
     pub address: u32,
+    // Size
     pub size: u32,
+    // 0, 0
     pub reserved: [u32; 2],
 }
 
+/// ERASE_REGION command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct EraseRegionCommand {
     pub base: CommandBase,
+    /// Address
     pub address: u32,
+    /// Size
     pub size: u32,
 }
 
-// Possibly move to other module
+/// Parameters of ReadFlash
+// TODO: Possibly move to other module
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct ReadFlashParams {
+    /// Flash offset
     pub address: u32,
+    /// Read size
     pub total_size: u32,
+    /// Size of each individual packet of data
     pub packet_size: u32,
+    /// Maximum number of un-acked packets
     pub max_inflight: u32,
 }
 
+/// READ_FLASH command input data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct ReadFlashCommand {
     pub base: CommandBase,
+    /// Read Flash parameters
     pub params: ReadFlashParams,
 }
 
+/// Direction of the packet
 #[allow(unused)]
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Direction {
+    /// Requests
     In = 0x00,
+    /// Responses
     Out = 0x01,
 }
 
+/// Response packet
+///
+/// See https://docs.espressif.com/projects/esptool/en/latest/esp32c2/advanced-topics/serial-protocol.html#id3
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C, packed(1))]
 pub struct Response<'a> {
+    /// Direction of the packet. Always 0x01 for responses.
     pub direction: Direction,
+    /// Same value as Command identifier in the request packet that triggered
+    /// the response
     pub command: CommandCode,
+    /// Size of data field. At least the length of the Status Bytes
     pub size: u16,
+    /// Response value used by READ_REG command. Zero otherwise.
     pub value: u32,
+    /// Status flag, success (0) or failure (1)
     pub status: u8,
+    /// If Status is 1, this indicates the type of error.
     pub error: u8,
+    // TODO: ROM loader has  4 bytes status (https://docs.espressif.com/projects/esptool/en/latest/esp32c2/advanced-topics/serial-protocol.html#status-bytes)
+    /// Variable length data payload. Length indicated by `size` field.
     pub data: &'a [u8],
 }
 
